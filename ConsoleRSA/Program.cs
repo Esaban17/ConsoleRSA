a4eaa4854bd429cff1a4e142fdb63a3e7ccec29b
JUAN SEBASTIAN SANCHEZ ROJAS <sanchez.iag2@gmail.com>
2020-11-13 15:21:04 -0600

    Wrote a simple test for the algorithm

--- ConsoleRSA/Program.cs
+++ ConsoleRSA/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleRSA
     {
         static void Main(string[] args)
         {
-           
+            RSA_Encryptor encryptor = new RSA_Encryptor();
+            int p = 61, q = 53;
+            var keys = encryptor.GenerateKeys(p, q);
+            int entry = 69;
+            int enc = encryptor.Cipher(entry, keys.e, keys.n);
+            int desenc = encryptor.Decipher(enc, keys.d, keys.n);
+
+            if (desenc == entry)
+            {
+                Console.WriteLine("Descifrado!!");
+            }
         }
     }
 }
