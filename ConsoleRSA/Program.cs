8fc01558a9befd3e0f15d7d02568769779bb55b5
JUAN SEBASTIAN SANCHEZ ROJAS <sanchez.iag2@gmail.com>
2020-11-16 15:41:30 -0600

    Added logic to cipher and decipher a file

--- ConsoleRSA/Program.cs
+++ ConsoleRSA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ConsoleRSA
 {
@@ -7,16 +8,36 @@ namespace ConsoleRSA
         static void Main(string[] args)
         {
             RSA_Encryptor encryptor = new RSA_Encryptor();
-            int p = 61, q = 53;
+            int p = 61, q = 53; // p * q siempre mayor a 255| 61 y 53
+            string name = "easy test.txt";
+            string[] fileName = name.Split('.');
             var keys = encryptor.GenerateKeys(p, q);
-            int entry = 69;
-            int enc = encryptor.Cipher(entry, keys.e, keys.n);
-            int desenc = encryptor.Decipher(enc, keys.d, keys.n);
 
-            if (desenc == entry)
-            {
-                Console.WriteLine("Descifrado!!");
-            }
+            //RUTA JUANSE
+            string filePath = @$"C:\Users\Usuario\Desktop\Archivos de Prueba\{name}";
+            string pathEncryption = @$"C:\Users\Usuario\Desktop\Archivos de Prueba\Encryptions";
+            string pathDecryption = @$"C:\Users\Usuario\Desktop\Archivos de Prueba\Decryptions";
+
+            var timer2 = new Stopwatch();
+            timer2.Start();
+            encryptor.Encrypt(filePath, fileName, pathEncryption, keys.e, keys.n);
+            encryptor.Decrypt($@"{pathEncryption}\{fileName[0]}.rsa", fileName, pathDecryption, keys.d, keys.n);
+            
+            timer2.Stop();
+            Console.WriteLine("TIME ELAPSED MILISECONDS: " + timer2.ElapsedMilliseconds);
+
+
+
+
+            //var keys = encryptor.GenerateKeys(p, q);
+            //int entry = 69;
+            //int enc = encryptor.Cipher(entry, keys.e, keys.n);
+            //int desenc = encryptor.Decipher(enc, keys.d, keys.n);
+
+            //if (desenc == entry)
+            //{
+            //    Console.WriteLine("Descifrado!!");
+            //}
         }
     }
 }
--- ConsoleRSA/RSA_Encryptor.cs
+++ ConsoleRSA/RSA_Encryptor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Numerics;
 using System.Text;
 
@@ -7,6 +8,10 @@ namespace ConsoleRSA
 {
     class RSA_Encryptor
     {
+        public static int buffer = 100000;
+        int largest = 0;
+
+        List<int> byteOutputList = new List<int>();
         public (int e, int d, int n) GenerateKeys(int p, int q)
         {
             int n = p * q;
@@ -14,6 +19,125 @@ namespace ConsoleRSA
             int e = GenerateE(phi);
             int d = GenerateD(phi, phi, e, 1, phi);
             return (e, d, n);
+        }
+        public void Encrypt(string filePath, string[] fileName, string pathEncryption, int key, int n)
+        {
+            using (var Fs = new FileStream(filePath, FileMode.Open))
+            {
+                using (var Br = new BinaryReader(Fs))
+                {
+                    using (var Fs2 = new FileStream($"{pathEncryption}/{fileName[0]}.rsa", FileMode.OpenOrCreate))
+                    {
+                        using (var Bw = new BinaryWriter(Fs2))
+                        {
+                            var bytes = new byte[buffer];
+                            while (Br.BaseStream.Position != Br.BaseStream.Length)
+                            {
+                                bytes = Br.ReadBytes(buffer);
+                                foreach (var item in bytes)
+                                {
+                                    int newVal = Cipher(item, key, n);
+                                    if (newVal > largest)
+                                    {
+                                        largest = newVal;
+                                    }
+                                    byteOutputList.Add(newVal);
+                                }
+                            }
+                            int maxBitSize = Convert.ToString(largest, 2).Length;
+                            string tempCode = "";
+                            Bw.Write(Convert.ToByte(maxBitSize));
+
+                            while (byteOutputList.Count > 0)
+                            {
+                                tempCode += Convert.ToString(byteOutputList[0], 2).PadLeft(maxBitSize, '0');
+                                byteOutputList.RemoveAt(0);
+                                if (byteOutputList.Count == 0)
+                                {
+                                    while (tempCode.Length >= 8)
+                                    {
+                                        var decimalNumber = Convert.ToInt32(tempCode.Substring(0, 8), 2);
+                                        var character = Convert.ToByte(decimalNumber);
+                                        Bw.Write(character);
+                                        tempCode = tempCode.Remove(0, 8);
+                                    }
+                                    if (tempCode.Length > 0)
+                                    {
+                                        tempCode = tempCode.PadRight(8, '0');
+                                        var decimalNumber = Convert.ToInt32(tempCode.Substring(0, 8), 2);
+                                        var character = Convert.ToByte(decimalNumber);
+                                        Bw.Write(character);
+                                        tempCode = tempCode.Remove(0, 8);
+                                    }
+                                }
+                                if (tempCode.Length >= 8)
+                                {
+                                    while (tempCode.Length >= 8)
+                                    {
+                                        var decimalNumber = Convert.ToInt32(tempCode.Substring(0, 8), 2);
+                                        var character = Convert.ToByte(decimalNumber);
+                                        Bw.Write(character);
+                                        tempCode = tempCode.Remove(0, 8);
+                                    }
+                                }
+                            }
+                            Bw.Close();
+                        };
+                        Fs2.Close();
+                    };
+                    Br.Close();
+                };
+                Fs.Close();
+            };
+        }
+        public void Decrypt(string pathEncryption, string[] fileName, string pathDecrypt, int key, int n)
+        {
+            using (var Fs = new FileStream(pathEncryption, FileMode.Open))
+            {
+                using (var Br = new BinaryReader(Fs))
+                {
+                    using (var Fs2 = new FileStream($"{pathDecrypt}/{fileName[0]}.txt", FileMode.OpenOrCreate))
+                    {
+                        using (var Bw = new BinaryWriter(Fs2))
+                        {
+                            List<int> outputValsList = new List<int>();
+                            var bytes = new byte[1];
+                            string tempVal = "";
+                            bytes = Br.ReadBytes(1);
+                            int maxBitSize = Convert.ToInt32(bytes[0]);
+                            while (Br.BaseStream.Position != Br.BaseStream.Length)
+                            {
+                                bytes = new byte[buffer];
+                                bytes = Br.ReadBytes(buffer);
+                                foreach (var item in bytes)
+                                {
+                                    tempVal += Convert.ToString(item, 2).PadLeft(8, '0');
+                                    if (tempVal.Length >= maxBitSize)
+                                    {
+                                        do
+                                        {
+                                            var val = Convert.ToInt32(tempVal.Substring(0, maxBitSize), 2);
+                                            outputValsList.Add(val);
+                                            tempVal = tempVal.Remove(0, maxBitSize);
+                                        } while (tempVal.Length >= maxBitSize);
+                                    }
+                                }
+                            }
+                            while (outputValsList.Count > 0)
+                            {
+                                Bw.Write(Convert.ToByte(Cipher(outputValsList[0], key, n)));
+                                outputValsList.RemoveAt(0);
+                            }
+
+
+                            Bw.Close();
+                        };
+                        Fs2.Close();
+                    };
+                    Br.Close();
+                };
+                Fs.Close();
+            };
         }
         private static IEnumerable<int> BruteForcePrimes(int max)
         {
@@ -89,17 +213,17 @@ namespace ConsoleRSA
             }
             return GenerateD(e, d, result1, result2, phi);
         }
-        public int Cipher(int entrada, int e, int n)
+        public int Cipher(int entry, int key, int n)
         {
-            BigInteger x = BigInteger.Pow(entrada, e);
+            BigInteger x = BigInteger.Pow(entry, key);
             BigInteger BigN = new BigInteger(n);
             return (int)(x % BigN);
         }
-        public int Decipher(int entrada, int d, int n)
-        {
-            BigInteger x = BigInteger.Pow(entrada, d);
-            var BigN = new BigInteger(n);
-            return (int)(x % BigN);
-        }
+        //public int Decipher(int entrada, int d, int n)
+        //{
+        //    BigInteger x = BigInteger.Pow(entrada, d);
+        //    var BigN = new BigInteger(n);
+        //    return (int)(x % BigN);
+        //}
     }
 }
